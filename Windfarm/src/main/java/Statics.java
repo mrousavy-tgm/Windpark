ba95113fff7c3b4cca5d34a71b284015e0eecbf9
Marc Rousavy <marcrousavy@hotmail.com>
2018-04-26 20:47:22 +0200

    Update Protocol

--- Windfarm/src/main/java/Statics.java
+++ Windfarm/src/main/java/Statics.java
@@ -4,5 +4,5 @@ public class Statics {
     public static final String USER = ActiveMQConnection.DEFAULT_USER;
     public static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
     public static final String URL = "http://192.168.152.148:61616";
-    public static final String SUBJECT = "Windpark";
+    public static final String SUBJECT = "Windfarm";
 }
