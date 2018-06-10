51636c667bde8c2382115a5fe58947cf8f4ea085
Marc Rousavy <marcrousavy@hotmail.com>
2018-06-10 14:31:54 +0200

    Implement JSON

--- DocumentOriented/wp-windpark/src/main/java/com/mrousavy/wpwindpark/WindparkRestController.java
+++ DocumentOriented/wp-windpark/src/main/java/com/mrousavy/wpwindpark/WindparkRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -15,9 +16,7 @@ public class WindparkRestController {
 
     private final String FILE_NAME = "parknodedata.xml";
 
-    @GetMapping("/xml")
-    @ResponseBody
-    public String getXml() {
+    public String readXml() throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
             StringBuilder builder = new StringBuilder();
 
@@ -26,7 +25,31 @@ public class WindparkRestController {
                 builder.append(sCurrentLine);
             }
 
-            String xml = builder.toString();
+            return builder.toString();
+        }
+    }
+
+    @GetMapping("/xml")
+    @ResponseBody
+    public String getXml() {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        try {
+            return readXml();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "<error>" + e.getMessage() + "</error>";
+        }
+    }
+
+
+    @GetMapping("/json")
+    @ResponseBody
+    public String getJson() {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        try {
+            String xml = readXml();
 
             JSONObject json = XML.toJSONObject(xml);
             String prettyJson = json.toString(4);
